47567dd327e6a2e6850a0cedd0f9cabb59ed1065
Ayesha Hafeez <ayesha.h@live.com>
2019-09-08 16:31:35 -0400

    fixed link for email verification

--- app/frontend/authentication/email.js
+++ app/frontend/authentication/email.js
@@ -1,8 +1,9 @@
 var { user_model} = require('../../models')
+var { domain } = require('../../../config')
 var mysql = require('../../../config/components/mysql.js')
 module.exports = async function confirm_email(req, res){
 	let key = req.params.key
 	let confirm = await user_model.confirm_email(key)
-	return res.redirect("http://" + mysql.host + "/signin?confirm=true")
+	return res.redirect( domain + "/signin?confirm=true")
 
 }
