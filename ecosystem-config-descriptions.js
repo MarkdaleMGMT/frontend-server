2a93705e4833ecabc4713067d983b232c3186313
Adam Zartin <a.zartin@mail.utoronto.ca>
2020-02-21 19:45:57 -0500

    description file for credentials

---
+++ ecosystem-config-descriptions.js
@@ -0,0 +1,9 @@
+/**
+ *  Add a key value pair to the exported dictionary below to show a description for a given credential
+ */
+module.exports = {
+    "ExampleCredential": "Example Credential Comment/Notes",
+    "GMAIL": "This is the email that the server will send emails from",
+    "GMAIL_PASS": "Password to the email you set for GMAIL",
+    "DOMAIN": "When email is sent by server to the user, this domain will be used as a link to the site"
+}
