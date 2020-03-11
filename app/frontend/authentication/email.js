e70b83dbc6ccf16668d204f48836a967fd27ddee
zartinad <zartinight@gmail.com>
2020-03-11 10:42:19 +0000

    fixed issue where email verfication redirects to correct place

--- app/frontend/authentication/email.js
+++ app/frontend/authentication/email.js
@@ -4,6 +4,6 @@ var mysql = require('../../../config/components/mysql.js')
 module.exports = async function confirm_email(req, res){
 	let key = req.params.key
 	let confirm = await user_model.confirm_email(key)
-	return res.redirect( domain + "/signin?confirm=true")
+	return res.redirect( domain + "/login?confirm=true")
 
 }
